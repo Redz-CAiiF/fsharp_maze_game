cdad044ec32185ea640789738125c9173f029277
Redz CAiiF <45766293+Redz-CAiiF@users.noreply.github.com>
2019-12-06 16:10:57 +0100

    ddd

---
+++ project/Program.fs
@@ -0,0 +1,39 @@
+﻿open System
+
+
+
+open globals
+open general
+open printing
+open map
+open maze
+
+
+//to do
+//A* to resolve the maze
+//printing the maze with the highlited solution path
+
+
+[<EntryPoint>]
+let main argv =
+
+    set_colour COLOUR_PLAYER COLOUR_BACKGROUND
+    let maze_tot = generate ROWS COLUMNS 3
+    reset_colour
+
+    //let custom_index = (index_general (get_maze_height maze_tot) (get_maze_width maze_tot))
+    //print_debug (get_maze_height maze_tot) (get_maze_width maze_tot) custom_index maze_tot
+    
+    let maze_tot_exp = expand maze_tot
+
+    print_map SET_THIN maze_tot_exp
+    //print_map SET_THICK maze_tot_exp
+    //print_map_generic (((get_sizes maze_tot_exp MAP_EXPANDED_TYPE) |> get_map_height)) (((get_sizes maze_tot_exp MAP_EXPANDED_TYPE) |> get_map_width)) maze_tot_exp
+
+    let maze_tot_exp_deloaded = expand (disconnect ROWS maze_tot)
+
+    print_map SET_THIN maze_tot_exp_deloaded
+
+    System.Console.ReadKey() |> ignore
+    0 // return exit code
+    
