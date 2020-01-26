56a5a601c1f95c61fb0f1dccba7e4d97dcf92078
DALore <58686790+whitedemond@users.noreply.github.com>
2020-01-26 01:33:51 +0100

    Add files via upload

---
+++ tests/MazeGUI.fs
@@ -0,0 +1,94 @@
+﻿(*
+*  FMaze - A .NET functional console game
+*
+*  (C) 2020
+*  Lorenzo Donatelli  (https://github.com/whitedemond)
+*  Matteo Agnoletto   (https://github.com/EPMatt)
+*  Matteo Libralesso  (https://github.com/Redz-CAiiF)
+*  
+*  For licensing conditions related to this project, see LICENSE
+*
+*)
+
+namespace FMaze.GUI
+open FMaze.Core
+open LabProg2019.Gfx
+open LabProg2019.Prelude
+open LabProg2019.Engine
+
+///<summary>The data structure representing an instance of a maze game rendered with the given Engine.<\summary>
+type MazeGUIType = {
+    ///<summary>the data structure representing the game logic.<\summary>
+    logic: MazeType
+    ///<summary>the data structure representing the expanded maze<\summary>
+    expanded_maze: ExpandedMazeType
+    ///<summary>the engine responsible for rendering the graphical user interface for the game.<\summary>
+    engine: engine
+    ///<summary>the sprite for the maze. Contains an image with pixels representing the current maze 2D structure.<\summary>
+    maze_sprite: sprite
+    ///<summary>the sprite for the player.<\summary>
+    player_sprite: sprite
+    ///current player position
+    player_position: (int*int)
+}
+
+///<summary>The <c>MazeGUI</c> module contains functions to operate on <code>MazeGUIType</code> instances. It exposes a constructor for generating a maze game with a graphical user interface.</summary>
+module MazeGUI = 
+
+    ///<summary>the pixel value which is used to represent walls in the graphical user interface.<\summary>
+    let WALL_PIXEL = pixel.create ('@',Color.White, Color.White)
+    ///the pixel value which is used to highlight the correct path in the graphical user interface
+    let PATH_PIXEL = pixel.create('@',Color.Green, Color.Green)
+    ///an empty and transparent pixel
+    let EMPTY_PIXEL = pixel.create(' ', Color.Black)
+    ///the image which is used to represent the player's position in the graphical user interface
+    let PLAYER_IMAGE = image.rectangle(1,1,pixel.create('@',Color.Yellow))
+    //offsets used for displaying the maze in the engine
+    let MAZE_X_OFFSET = 5
+    let MAZE_Y_OFFSET = 5
+
+    ///<summary>Convert an ExpandedMaze to a pixel array.</summary>
+    ///<param name="expanded_maze">The expanded maze that we want to convert</param>
+    ///<returns>The pixel map.</returns>
+    let to_pixel_array (expanded_maze: ExpandedMazeType): pixel[] =
+        let arr = Array.map (fun t -> if t = Walls.CLOSED then WALL_PIXEL else EMPTY_PIXEL) (Array.ofList expanded_maze.map)
+        arr
+    
+    ///<summary>Convert an ExpandedMaze to an image.</summary>
+    ///<param name="expanded_maze">The expanded maze that we want to convert</param>
+    ///<returns>An image from the expanded maze.</returns>
+    let to_image (expanded_maze: ExpandedMazeType): image =
+        new image (expanded_maze.cols, expanded_maze.rows, to_pixel_array expanded_maze)
+    
+    
+    let solution_to_image (solution: SolutionType) : image =
+        let pxs = Array.create (solution.map_cols*solution.map_rows) EMPTY_PIXEL
+        List.iter (fun e -> pxs.[e] <- PATH_PIXEL) solution.path
+        new image (solution.map_cols,solution.map_rows,pxs)
+
+    let display_solution (maze : MazeGUIType) :unit =
+       // let sol =Maze.Resolutor.solve maze.expanded_maze    //test:solve the maze
+        //ignore (maze.engine.create_and_register_sprite(solution_to_image (sol),MAZE_X_OFFSET,MAZE_Y_OFFSET,10)) //this would be the correct render
+        ignore (maze.engine.create_and_register_sprite(image.rectangle(5,5,PATH_PIXEL),MAZE_X_OFFSET,MAZE_Y_OFFSET,10)) //render a sample rectangle just to check the engine is working
+    
+    let get_x player_position = snd player_position
+
+    let get_y player_position = fst player_position
+    
+
+    ///<summary>Creates a new MazeGUI from the given parameters. It generates and initializes all the necessary items to handle a graphical maze game.</summary>
+    ///<param name="rows">The rows of the mazegui</param>
+    ///<param name="cols">The cols of the mazegui</param>
+    ///<returns>The MazeGUI instance created with the given parameters.</returns>
+    let create (rows: int) (cols:int): MazeGUIType =
+        let logic =Maze.create rows cols
+        let expanded_maze = Maze.Expand.expand logic
+        let engine= new engine(expanded_maze.cols+10, expanded_maze.rows+10)
+        {
+            logic=logic;
+            expanded_maze= expanded_maze;
+            engine=engine;
+            maze_sprite=engine.create_and_register_sprite(to_image expanded_maze, MAZE_X_OFFSET,MAZE_Y_OFFSET,0);
+            player_sprite=engine.create_and_register_sprite(PLAYER_IMAGE, snd expanded_maze.start+MAZE_X_OFFSET, fst expanded_maze.start+MAZE_Y_OFFSET ,0);
+            player_position = expanded_maze.start
+        }
---
+++ tests/Program.fs
@@ -0,0 +1,96 @@
+﻿(*
+* LabProg2019 - Progetto di Programmazione a.a. 2019-20
+* Program.fs - game entry point
+* (C) 2019 Alvise Spano' @ Universita' Ca' Foscari di Venezia
+*)
+
+module FMaze.Program
+
+open System
+open System.Diagnostics
+open LabProg2019.Globals
+open LabProg2019
+open LabProg2019.Gfx
+open System.IO.Pipes
+open System.IO
+open FMaze.GUI
+
+// game mode (client)
+//
+  
+let main_game () =
+    use p = new Process ()
+    p.StartInfo.UseShellExecute <- true
+    p.StartInfo.CreateNoWindow <- false
+    p.StartInfo.Arguments <- "1"
+    p.StartInfo.FileName <- Process.GetCurrentProcess().MainModule.FileName
+    ignore <| p.Start ()
+
+    use client = new NamedPipeClientStream (".", Config.log_pipe_name, PipeDirection.Out)
+    client.Connect ()
+    Log <- new remote_logger (client)
+
+    let main ()=
+        let gui = MazeGUI.create 10 30
+        let next_position = gui.logic.
+
+        let my_update (key : ConsoleKeyInfo) (screen : wronly_raster) (state : MazeGUIType) =
+            let dx, dy = 
+                match key.KeyChar with
+                |'w' -> if(wallon) then 0.,1. else 0.,0.
+                |'s' -> if(wallon) then 0.,-1. else 0.,0.
+                |'a' -> if(wallon) then -1.,0. else 0.,0.
+                |'d' -> if(wallon) then 1.,0. else 0.,0.
+                | _  -> 0.,0.
+            state.player_sprite.move_by(dx, dy)    
+            state, key.KeyChar='q'
+        gui.engine.loop_on_key my_update gui
+
+    main ()
+    0
+
+// log mode (server)
+//
+
+let main_log_server () =
+    Log.msg "log server process started"
+    Console.Title <- Config.log_console_title
+    let server = new NamedPipeServerStream (Config.log_pipe_name, PipeDirection.In)
+    Log.msg "waiting for incoming connection..."
+    server.WaitForConnection ()
+    Log.msg "client connected"
+    use r = new StreamReader (server)
+    while not r.EndOfStream do
+        try
+            let fg = r.ReadLine ()
+            let parse c = Enum.Parse (typeof<Color>, c) :?> Color
+            let s = r.ReadLine().Replace (Config.log_pipe_translate_eol, '\n')
+            Console.ForegroundColor <- parse fg
+            Console.WriteLine s
+            Console.ResetColor ()
+        with e -> Log.error "exception caught:%s\nstack trace: %O" e.Message e
+
+    Log.warn "EOF reached: quitting."
+    0
+
+// main 
+//
+
+[<EntryPoint>]
+let main argv = 
+    #if TEST
+    Test.main ()
+    printfn "\nPress any key to quit..."
+    Console.ReadKey () |> ignore
+    0
+    #else
+    if argv.Length > 0 then 
+        main_log_server ()
+    else
+        let code = main_game ()
+        printfn "\nPress any key to quit..."
+        Console.ReadKey () |> ignore
+        code
+    #endif
+
+
